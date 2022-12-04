d9adfed54b574798f2905239ac0f6bf9477f8c83
Jana <janamurugesan01@gmail.com>
2022-12-04 15:22:10 +0530

    List view created and new activity_notes_add.xml added

---
+++ app/src/main/java/com/jana/todo/NotesAdd.kt
@@ -0,0 +1,11 @@
+package com.jana.todo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NotesAdd : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notes_add)
+    }
+}
